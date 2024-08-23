52b2e5e15af8a9032c05a74798a7589a99aa9fbb
ChugGitH <chung5800h@gmail.com>
2024-08-23 08:05:53 +0700

    Add files via upload

---
+++ part12.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+echo "This is a new PHP script example";
+echo '<h1 style="color: green; text-align: center;">Welcome to TMU</h1>';
+echo '<p style="font-size: 18px; text-align: center;">Learning PHP </p>';
+echo '<p style="font-size: 18px; text-align: center;">This is a test of image</p>';
+echo '<img src="https://hoanghamobile.com/tin-tuc/wp-content/uploads/2023/07/hinh-dep.jpg" alt="Example Image" />';
+
+?>
+
+<p style="text-align: center;">Have a great day!</p>
+
+</body>
+</html>
